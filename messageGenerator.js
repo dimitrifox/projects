16f02e37c0135d23b2e3fc4f9a987ff1e10538de
dimitrifox <puretie13@gmail.com>
2020-12-20 14:40:19 +0500

    Used arrays instead of switch

--- messageGenerator.js
+++ messageGenerator.js
@@ -1,70 +1,14 @@
-let firsText = Math.floor(Math.random() * 4);
-switch (firsText) {
-    case 0:
-        firsText = 'I like ';
-        // console.log('I like ');
-        break;
-    case 1:
-        firsText = 'I have to ';
-        // console.log('I hate ');
-        break;
-    case 2:
-        firsText = 'I used to ';
-        // console.log('I have to ');
-        break;
-    case 3:
-        firsText = 'I used to ';
-        // console.log('I used to ');
-        break;
-    default:
-        console.log('This is default value...');
-        break;
-};
+const firstPart = ['I like ', 'I have to ', 'I used to ', 'I hate '];
+let firsRandText = Math.floor(Math.random() * firstPart.length);
+let firstText = firstPart[firsRandText];
 
-let secondText = Math.floor(Math.random() * 4);
-switch (secondText) {
-    case 0:
-        secondText = 'play ';
-        // console.log('play ');
-        break;
-    case 1:
-        secondText = 'eat ';
-        // console.log('eat ');
-        break;
-    case 2:
-        secondText = 'wash ';
-        // console.log('wash ');
-        break;
-    case 3:
-        secondText = 'make ';
-        // console.log('make ');
-        break;
-    default:
-        console.log('This is default value...');
-        break;
-};
+const secondPart = ['play ', 'eat ', 'wash ', 'make '];
+let secondRandText = Math.floor(Math.random() * secondPart.length);
+let secondText = secondPart[secondRandText];
 
-let thirdText = Math.floor(Math.random() * 4);
-switch (thirdText) {
-    case 0:
-        thirdText = 'game.'
-        // console.log('game.');
-        break;
-    case 1:
-        thirdText = 'dishes.'
-        // console.log('dishes.');
-        break;
-    case 2:
-        thirdText = 'homework.'
-        // console.log('homework.');
-        break;
-    case 3:
-        thirdText = 'sleep.'
-        // console.log('sleep.');
-        break;
-    default:
-        console.log('This is default value...');
-        break;
-};
+const thirdPart = ['game.', 'dishes.', 'homework.', 'sleep.'];
+let thirdRandText = Math.floor(Math.random() * thirdPart.length);
+let thirdText = thirdPart[thirdRandText];
 
-console.log(firsText + secondText + thirdText);
+
+console.log(firstText + secondText + thirdText);
